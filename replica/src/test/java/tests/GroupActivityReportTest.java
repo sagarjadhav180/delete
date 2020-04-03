6a9941abf3e01903b1b1f656468b130716ab16a0
sagarjadhav180 <sagar.j@moentek.com>
2020-04-03 19:36:51 +0530

    UI verification , Pagination toolbox , pagination toolbox count, tile count functions added

---
+++ replica/src/test/java/tests/GroupActivityReportTest.java
@@ -0,0 +1,126 @@
+package tests;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import constants.Constants;
+import pom.GroupActivityReportsPage;
+import pom.HomePage;
+import pom.LoginPage;
+
+public class GroupActivityReportTest extends TestBase {
+	
+	GroupActivityReportsPage ga;
+	HomePage hp;
+	
+		
+	@BeforeClass
+	public void goToGroupActivityReportPage() throws InterruptedException, IOException{
+	
+		LoginPage lp=new LoginPage(driver);
+        logger=extent.startTest("validLogin"); 
+        logger.log(LogStatus.INFO, "verifying login with valid username_and_password. ");
+        logger.assignCategory("Login Suite");
+        lp.validLogin();
+        
+        hp=new HomePage(driver);
+        hp.left_hand_navigation_bar_click();
+		hp.clickAction(Constants.HomePage.legacy_reports);
+		
+		hp.click_subLink(Constants.HomePage.group_activity_report);
+	}
+
+	//ui verification of groupActivity reports page
+	@Test(priority=1)
+	public void uiVerification() throws InterruptedException{
+		logger=extent.startTest("uiVerification Test");
+		logger.assignCategory(Constants.group_activity_report_category);
+			
+		ga=GroupActivityReportsPage.instance();
+		ga.UIVerification();
+	}
+	
+	//Tile count verification of groupActivity reports page
+	@Test(priority=2)
+	public void tileCountVerification() throws InterruptedException{
+		logger=extent.startTest("Tile Count Verification");
+		logger.assignCategory(Constants.group_activity_report_category);
+			
+		ga=GroupActivityReportsPage.instance();
+		ga.tileCountVerification();
+	}
+	
+	//Table count verification of groupActivity reports page
+	@Test(priority=3)
+	public void tableCountVerification() throws InterruptedException{
+		logger=extent.startTest("Table Count Verification");
+		logger.assignCategory(Constants.group_activity_report_category);
+			
+		ga=GroupActivityReportsPage.instance();
+		ga.tableCount();
+	}
+	
+	//Pagination UI verification of groupActivity reports page
+	@Test(priority=4)
+	public void paginationToolboxUIVerification() throws InterruptedException{
+		logger=extent.startTest("Pagination Toolbox UI Verification");
+		logger.assignCategory(Constants.group_activity_report_category);
+			
+		ga=GroupActivityReportsPage.instance();
+		ga.paginationUI();
+	}
+	
+	//Pagination count verification of groupActivity reports page
+	@Test(priority=5)
+	public void paginationCountVerification() throws InterruptedException{
+		logger=extent.startTest("Pagination Count Verification");
+		logger.assignCategory(Constants.group_activity_report_category);
+			
+		ga=GroupActivityReportsPage.instance();
+		ga.paginationCount();
+	}
+	
+	
+	
+	
+	
+	
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//logging out -tear down
+		@AfterClass
+		public void logOut() throws IOException{
+			
+			LoginPage lp=new LoginPage(driver);
+			logger=extent.startTest("LogOut"); 
+			logger.log(LogStatus.INFO, "loggin out.. ");
+			lp.logOut();
+			        		
+		}
+		
+		
+		
+}
