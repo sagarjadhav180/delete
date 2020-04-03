72338b28d0262814d86df001de4ee2d95c774144
sagarjadhav180 <sagar.j@moentek.com>
2020-04-03 12:30:45 +0530

    Constants class added

---
+++ replica/src/main/java/constants/Constants.java
@@ -0,0 +1,57 @@
+package constants;
+
+public class Constants {
+
+	
+	public static class LegacyReports{
+		
+		//call details
+		//Action section
+		public static final String play_button="Listen to call";
+		public static final String inforamtion_icon_button="Toggle Call Info";
+		public static final String block_number="Block Number";
+		public static final String download_audio="Download Audio File";
+		public static final String email_call="Email Call";
+		public static final String default_mail_id_from="no-reply@messages.services";
+		public static final String default_mail_id_to="sagar.j@yopmail.com";
+		public static final String default_mail_id_message="Test Automation call mail";
+		
+		//filters
+		public static final String ad_source="Ad Source";
+		public static final String caller_id="Caller ID";
+		public static final String tracking_number="Tracking Number";		
+		public static final String duration="Duration";
+		public static final String group_name="Group Name";
+		public static final String disposition="Disposition";
+		public static final String line_type="Line Type";
+		
+		//Date range
+		public static final String today="Today";
+		public static final String yesterday="Yesterday";
+		public static final String last_7_days="Last 7 Days";
+		public static final String last_30_days="Last 30 Days";
+		
+	}
+	
+    public static class HomePage{
+		
+	}
+    
+    public static class LoginPage{
+    	
+    }
+    
+    public static class Campaign{
+    	
+    }
+   
+	public static class TrackingNumber{
+		
+	}
+	
+	
+}
+
+	
+	
+
