6223b971782b0b22bdb041a953282b2a18ba9e6b
sagarjadhav180 <sagar.j@moentek.com>
2020-04-22 11:24:06 +0530

    TestBase class modified

--- replica/src/test/java/tests/TestBase.java
+++ replica/src/test/java/tests/TestBase.java
@@ -124,7 +124,7 @@ public class TestBase
 		//to delete cookies
 	    driver.manage().deleteAllCookies();
 	    driver.get(url_to_hit);
-	    TestData.createData();			    
+//	    TestData.createData();			    
 	   
 	    
 	}
