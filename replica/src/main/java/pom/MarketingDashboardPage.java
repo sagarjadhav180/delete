4b2b0c77e600a4b29368300eef2d115583e5cc17
sagarjadhav180 <sagar.j@moentek.com>
2020-05-18 19:31:54 +0530

    Looker reports pom functions added

--- replica/src/main/java/pom/MarketingDashboardPage.java
+++ replica/src/main/java/pom/MarketingDashboardPage.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
 import com.relevantcodes.extentreports.LogStatus;
 
 import tests.TestBase;
+import tests.Util;
 
 public class MarketingDashboardPage extends TestBase{
 	
@@ -34,9 +36,85 @@ public class MarketingDashboardPage extends TestBase{
 
 	@FindBy(xpath="//strong[@class='filter-section-title']//i")
 	private WebElement filter_button;
+
+	@FindBy(xpath="//div[@class='looker-vis-context-title']//span[text()='Calls Mix']")
+	private WebElement call_mix_label;
+
+	@FindBy(xpath="(//div[starts-with(@id,'highcharts-')])[1]")
+	private WebElement call_mix_pie_chart;
+
+	@FindBy(xpath="(//div[starts-with(@id,'highcharts-')])[2]")
+	private WebElement highest_converting_graph;
+
+	@FindBy(xpath="(//div[starts-with(@id,'highcharts-')])[3]")
+	private WebElement total_calls_graph;
+
+	@FindBy(xpath="(//div[starts-with(@id,'highcharts-')])[4]")
+	private WebElement unique_calls_graph;
+
+	@FindBy(xpath="(//div[@class='grid-element']//div[@class='ag-header-row'])[2]//span[@class='column-label']//strong")
+	private List<WebElement> marketing_dashboard_table_columns;
+
+	String[] expected_marketing_dashboard_table_columns={"Calls By Group","Total Calls","Leads","Lead % of Calls","Conversions","Conversion % of Calls","Total Duration","Average Duration"};
+
+	@FindBy(xpath="(//span[text()='Detailed View']//parent::div[@class='looker-vis-context-title'])[1]")
+	private WebElement marketing_dashboard_detailed_view_button;
+	
+	@FindBy(xpath="//p[text()='This report is intended for download and/or scheduling only. Due to the amount of data available, it may take longer than usual to load. To increase loading speed, the amount of records available here has been reduced to 100. All records for the Date Range selected will be available on the downloaded or scheduled version.']")
+	private WebElement marketing_dashboard_detailed_view_note;
+
+	@FindBy(xpath="//div[@class='centered']")
+	private WebElement default_report_tile_marketing_dashboard_detailed_view_page;
+
+	@FindBy(xpath="//div[@class='looker-vis-context-title']//span[text()='Highest Converting']")
+	private WebElement highest_converting_map_label;
+
+	@FindBy(xpath="(//div[@class='grid-element']//div[@class='ag-header-row'])[2]//span[@class='column-label']//strong")
+	private List<WebElement> website_calls_table_columns;
+
+	String[] expected_website_calls_table_columns={"Calls By Group","Website Number Swaps","Website Calls","% of Website Number Swaps","Leads","Lead % of Website Calls","Conversions","Conversion % of Website Calls","Total Duration","Average Duration"};
+
+	@FindBy(xpath="(//span[text()='Detailed View']//parent::div[@class='looker-vis-context-title'])[2]")
+	private WebElement website_calls_detailed_view_button;
+	
+	@FindBy(xpath="//p[text()='This report is intended for download and/or scheduling only. Due to the amount of data available, it may take longer than usual to load. To increase loading speed, the amount of records available here has been reduced to 100. All records for the Date Range selected will be available on the downloaded or scheduled version.']")
+	private WebElement website_calls_detailed_view_note;
+
+	@FindBy(xpath="//div[@class='centered']")
+	private WebElement default_report_tile_website_calls_detailed_view_page;
+	
+	@FindBy(xpath="(//div[@class='looker-vis-context-title']//span[text()='Website Number Swaps'])[3]")
+	private WebElement website_number_swaps_label;
+
+	@FindBy(xpath="(//div[@class='looker-vis-context-title']//span[text()='Website Number Swaps'])[3]")
+	private WebElement website_calls_label;
+
+	@FindBy(xpath="(//div[@class='looker-vis-context-title']//span[text()='Website Calls'])[2]")
+	private WebElement website_calls_map_label;
+
+	@FindBy(xpath="(//div[@class='looker-vis-context-title']//span[text()='Website Number Swaps'])[3]")
+	private WebElement website_number_swaps_map_label;
+
+	@FindBy(xpath="(//div[@class='looker-leaflet-container'])[1]")
+	private WebElement website_calls_map;
+
+	@FindBy(xpath="(//div[@class='looker-leaflet-container'])[2]")
+	private WebElement website_number_swaps_map;
 	
 	@FindBy(xpath="//table[@class='explore-filters clearfix']//tbody//tr//td[@class='filter-name']")
 	private List<WebElement> filter_elements_after_expanding;
+	
+	String[] expected_filter_elements_after_expanding={"Date Range","Show Columns By","Stack Columns By","Group Calls By","Group Website Calls By","Ad Source","Campaign","Group","Keywords","Referring Source","Referring Type","Ring-to Name","Ring-to Number","Tracking Number Name","Tracking Number"};
+	
+	@FindBy(xpath="(//div[@class='vis-single-value']//span[@class='looker-vis-context-title-text '][text()='Detailed View'])[1]")
+	private WebElement mkt_dashboard_tile;
+		
+	String[] expected_tile_names_mkt_dashboard={"Total Calls","Leads","Conversions"};
+
+	@FindBy(xpath="(//div[@class='vis-single-value']//span[@class='looker-vis-context-title-text '][text()='Detailed View'])[2]")
+	private WebElement website_calls_tile;
+	
+	String[] expected_tile_names_website_calls={"Website Number Swaps","Website Calls"};
 	
 	SoftAssert softassert=new SoftAssert(); 
 
@@ -92,5 +170,40 @@ public class MarketingDashboardPage extends TestBase{
 		Assert.assertTrue(timezone.isDisplayed(),"Time Zone is not present or locator has been changed.");
 	}
 
+    public void filterButton(){
+		
+		wait.until(ExpectedConditions.visibilityOf(filter_button));
+	    logger.log(LogStatus.INFO, "verifying if filter_button is present");
+	    softassert.assertTrue(filter_button.isDisplayed(), "filter_button is not present");
+
+	    logger.log(LogStatus.INFO, "verifying if filter_button is enabled");
+	    softassert.assertTrue(filter_button.isEnabled(), "filter_button is not enabled");	    
+	}
+    
+public void filterElements(){
+        
+    	Util.click(filter_button);
+        
+        for(int k=0;k<filter_elements_after_expanding.size();){
+        	for(int j=0;j<expected_filter_elements_after_expanding.length;j++){
+
+        		if(filter_elements_after_expanding.get(k).getText().equals(expected_filter_elements_after_expanding[j])){
+    	    			    			    		
+        		wait.until(ExpectedConditions.visibilityOf(filter_elements_after_expanding.get(k)));	    		
+        		System.out.println("we-"+filter_elements_after_expanding.get(k).getText());
+        		System.out.println("array-"+expected_filter_elements_after_expanding[j]);		
+        		logger.log(LogStatus.INFO,"verifying if "+expected_filter_elements_after_expanding[j]+" filter is present");
+        	    softassert.assertEquals(filter_elements_after_expanding.get(k).getText(),expected_filter_elements_after_expanding[j],expected_filter_elements_after_expanding[j]+" filter element is npt present");
+        		}
+        		}
+        	k++;
+        }
+
+        softassert.assertAll();
+    	//collapsing filter section
+    	Util.click(filter_button);
+
+    }
+    
 
 }
--- replica/src/test/java/tests/MarketingDashboardTest.java
+++ replica/src/test/java/tests/MarketingDashboardTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import com.relevantcodes.extentreports.LogStatus;
 
 import constants.Constants;
+import pom.CallBackReportPage;
 import pom.CallLogsReportPage;
 import pom.HomePage;
 import pom.IVRKeypressReportPage;
@@ -22,21 +23,26 @@ public class MarketingDashboardTest extends TestBase {
 	MarketingDashboardPage mkt;
 	
 	@BeforeClass
-	public void goToLookerReports() throws InterruptedException, IOException{
+	public void goToLookerReports() throws Exception{
 		LoginPage lp=new LoginPage(driver);
 		logger=extent.startTest("validLogin"); 
         logger.log(LogStatus.INFO, "verifying login with valid username_and_password. ");
         lp.validLogin();
         
-		hp=new HomePage(driver);
-		hp.clickAction("Reports");
-		Thread.sleep(7000);
-		clr.goToReport(Constants.LookerReports.marketing_dashboard_report);
-
-		clr.switchToIFrame();
-
+        goToReport();
 	}
 
+	public void goToReport() throws Exception{
+		mkt=new MarketingDashboardPage(driver);
+		clr=new CallLogsReportPage(driver);
+		hp=new HomePage(driver);
+		hp.clickAction("Reports");
+		Thread.sleep(1000);
+		clr.goToReport(Constants.LookerReports.marketing_dashboard_report);
+        Thread.sleep(7000);
+		mkt.switchToIFrame();
+	}
+	
 	@Test(priority=1)
 	public void headerLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Header Label Verification Test"); 
@@ -69,9 +75,25 @@ public class MarketingDashboardTest extends TestBase {
 		mkt.presenceOfTimeZone();
 	}
 	
+	public void filterButtonTest() throws IOException, InterruptedException{
+		logger=extent.startTest("Filter Button Test"); 
+		logger.assignCategory(Constants.marketing_dashboard_category);
+		mkt=new MarketingDashboardPage(driver);
+        mkt.filterButton();
+	}
+	
+	public void filterButtonElementsTest() throws IOException, InterruptedException{
+		logger=extent.startTest("Filter Button Elements Test"); 
+		logger.assignCategory(Constants.marketing_dashboard_category);
+		mkt=new MarketingDashboardPage(driver);
+        mkt.filterElements();
+	}
 	
 	@AfterClass
 	public void loggingOut() throws InterruptedException{
+		
+		mkt=new MarketingDashboardPage(driver);
+		mkt.switchToMainWindow();
 		
 		LoginPage lp=new LoginPage(driver);
         logger=extent.startTest("LogOut"); 
