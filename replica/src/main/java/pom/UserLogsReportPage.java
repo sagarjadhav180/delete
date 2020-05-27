18064baf0d457c2add5e18d5c19c8b1acf925aec
sagarjadhav180 <sagar.j@moentek.com>
2020-05-27 16:18:34 +0530

    Looker Reports pom functions added

--- replica/src/main/java/pom/UserLogsReportPage.java
+++ replica/src/main/java/pom/UserLogsReportPage.java
@@ -2,22 +2,29 @@ package pom;
 
 import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
 import com.relevantcodes.extentreports.LogStatus;
 
 import tests.TestBase;
+import tests.Util;
 
 public class UserLogsReportPage extends TestBase{
 
 	@FindBy(xpath="//button[@class='btn run-button embed-view btn-primary'][text()='Run']")
 	private WebElement run_button;
 
+	@FindBy(xpath="(//div[@class='title-main']//span[text()='User Logs'])")
+	private WebElement header_label;
+	
 	@FindBy(xpath="//div[@class='dropdown-toggle button-xs']/i")
 	private WebElement gear_icon;
 
@@ -38,6 +45,33 @@ public class UserLogsReportPage extends TestBase{
 	@FindBy(xpath="//table[@class='explore-filters clearfix']//tbody//tr//td[@class='filter-name']")
 	private List<WebElement> filter_elements_after_expanding;
 	
+	String[] expected_filter_elements_after_expanding={"Date Range","Show Activity By","Activity","Group","User"};
+	
+	@FindBy(xpath="(//div[@class='vis-header'])[1]//span[text()='All Activity by Type']")
+	private WebElement all_activity_by_type_label;	
+
+	@FindBy(xpath="(//div[starts-with(@id,'highcharts-')])[1]")
+	private WebElement all_activity_by_type_graph;	
+	
+	@FindBy(xpath="(//div[@class='ag-grid-container'])[1]//div[@class='ag-header-container']//strong")
+	private List<WebElement> all_activity_table_columns;	
+	
+	String[] expected_all_activity_table_columns={"Activity Date/Time","User(Name/Login)","Group","Activity","Activity Details"};
+
+	@FindBy(xpath="//div[@class='title-and-subtitle']//div[text()='Login Activity']")
+	private WebElement login_activity_table_label;
+
+	@FindBy(xpath="(//div[@class='vis-header']//span[text()='Login Activity'])[1]")
+	private WebElement login_activity_graph_label;
+
+	@FindBy(xpath="(//div[starts-with(@id,'highcharts-')])[2]")
+	private WebElement login_activity_graph;	
+
+	@FindBy(xpath="(//div[@class='ag-grid-container'])[2]//div[@class='ag-header-container']//strong")
+	private List<WebElement> login_activity_table_columns;
+	
+	String[] expected_login_activity_table_columns={"Activity By","Login Count"};
+	
 	SoftAssert softassert=new SoftAssert(); 
 
 	public UserLogsReportPage(WebDriver driver){
@@ -56,9 +90,18 @@ public class UserLogsReportPage extends TestBase{
 		
 	}
     
-	public void headerLabel(){
+    public void runButton(){
 
-				
+		wait.until(ExpectedConditions.visibilityOf(run_button));
+		logger.log(LogStatus.INFO, "verifying if run_button is present");
+		Assert.assertTrue(run_button.isDisplayed(),"run_button is not displayed or locator has been chamged..");
+		logger.log(LogStatus.INFO, "verifying if run_button is enabled");
+		Assert.assertTrue(run_button.isEnabled(),"run_button is not enabled");
+	}
+    
+	public void headerLabel(){
+		logger.log(LogStatus.INFO, "Verifying if header label is present");
+		Assert.assertTrue(header_label.isDisplayed(),"header label is not displayed or locator changed");
 	}
     
     public void presenceOfGearIcon(){
@@ -84,6 +127,7 @@ public class UserLogsReportPage extends TestBase{
 		}
     
 		softassert.assertAll();
+		gear_icon.click();
     }
 
     public void presenceOfTimeZone(){
@@ -92,4 +136,167 @@ public class UserLogsReportPage extends TestBase{
 		Assert.assertTrue(timezone.isDisplayed(),"Time Zone is not present or locator has been changed.");
 	}
 	
+    public void allActivityByTypeLabel(){
+		
+		logger.log(LogStatus.INFO, "Verifying if All Activity By Type Label is present");
+		Assert.assertTrue(all_activity_by_type_label.isDisplayed(),"All Activity By Type label is not present or locator has been changed.");
+	}
+    
+    public void allActivityByTypeGraph(){
+
+    	logger.log(LogStatus.INFO, "Verifying if All Activity By Type Graph is present");
+    	Assert.assertTrue(all_activity_by_type_graph.isDisplayed(),"All Activity By Type Graph is not present or locator has been changed.");    		
+	}
+    
+    public void allActivityTableColumnVerification() throws InterruptedException{
+		
+		Thread.sleep(2000);
+		
+		for(int i=0;i<all_activity_table_columns.size();i++){
+			
+			for(int j=0;j<expected_all_activity_table_columns.length;j++){
+				
+				if(all_activity_table_columns.get(i).getText().equals(expected_all_activity_table_columns[j])){
+					
+					logger.log(LogStatus.INFO, "Verifying if "+expected_all_activity_table_columns[j]+" is present");
+					softassert.assertTrue(all_activity_table_columns.get(i).getText().equals(expected_all_activity_table_columns[j]),"Column "+expected_all_activity_table_columns[j]+" is not present");
+				}
+			}
+			
+		}
+			
+		softassert.assertAll();
+	}
+    
+    public void loginActivityTableLabel(){
+		
+		logger.log(LogStatus.INFO, "Verifying if Login Activity Table Label is present");
+		Assert.assertTrue(login_activity_table_label.isDisplayed(),"Login Activity Table label is not present or locator has been changed.");
+	}
+    
+    public void loginActivityGraphLabel(){
+		
+		logger.log(LogStatus.INFO, "Verifying if Login Activity Graph Label is present");
+		Assert.assertTrue(login_activity_graph_label.isDisplayed(),"Login Activity Graph label is not present or locator has been changed.");
+	}
+    
+    public void loginActivityGraph(){
+
+    	logger.log(LogStatus.INFO, "Verifying if Login Activity Graph is present");
+    	Assert.assertTrue(login_activity_graph.isDisplayed(),"Login Activity Graph is not present or locator has been changed.");    		
+	}    
+    
+    public void loginActivityTableColumnVerification() throws InterruptedException{
+		
+		Thread.sleep(2000);
+		
+		for(int i=0;i<login_activity_table_columns.size();i++){
+			
+			for(int j=0;j<expected_login_activity_table_columns.length;j++){
+				
+				if(login_activity_table_columns.get(i).getText().equals(expected_login_activity_table_columns[j])){
+					
+					logger.log(LogStatus.INFO, "Verifying if "+expected_login_activity_table_columns[j]+" is present");
+					softassert.assertTrue(login_activity_table_columns.get(i).getText().equals(expected_login_activity_table_columns[j]),"Column "+expected_login_activity_table_columns[j]+" is not present");
+				}
+			}
+			
+		}
+			
+		softassert.assertAll();
+	}
+    
+    public void filterElements(){
+        
+    	Util.click(filter_button);
+        
+        for(int k=0;k<filter_elements_after_expanding.size();){
+        	for(int j=0;j<expected_filter_elements_after_expanding.length;j++){
+
+        		if(filter_elements_after_expanding.get(k).getText().equals(expected_filter_elements_after_expanding[j])){
+    	    			    			    		
+        		wait.until(ExpectedConditions.visibilityOf(filter_elements_after_expanding.get(k)));	    		
+        		System.out.println("we-"+filter_elements_after_expanding.get(k).getText());
+        		System.out.println("array-"+expected_filter_elements_after_expanding[j]);		
+        		logger.log(LogStatus.INFO,"verifying if "+expected_filter_elements_after_expanding[j]+" filter is present");
+        	    softassert.assertEquals(filter_elements_after_expanding.get(k).getText(),expected_filter_elements_after_expanding[j],expected_filter_elements_after_expanding[j]+" filter element is npt present");
+        		}
+        		}
+        	k++;
+        }
+
+        softassert.assertAll();
+    	//collapsing filter section
+    	Util.click(filter_button);
+
+    }
+    
+    public void filterFeatureForAllActivityTable(String filterName) throws InterruptedException{
+
+    		int index=0;
+            String filterValue;
+    		for(int i=0;i<all_activity_table_columns.size();i++){
+    			
+    			if(filterName.startsWith(all_activity_table_columns.get(i).getText())){
+    				
+    				index=i+1;
+    				break;
+    			}
+    		}
+    		List<WebElement> value_to_be_filtered = driver.findElements(By.xpath("(//div[@class='ag-grid-container'])[1]//div[@class='ag-center-cols-container']/div/div["+String.valueOf(index)+"]"));		
+    		filterValue=value_to_be_filtered.get(0).getText();
+    		
+    		filter_button.click();
+    		
+    		String xpath="//table[@class='explore-filters clearfix']//tbody//tr//td[@class='filter-name'][text()='"+filterName+"']";
+    		
+    		WebElement filter_textbox = driver.findElement(By.xpath(""+xpath+"//parent::tr//select//following-sibling::span"));
+    	
+    		wait.until(ExpectedConditions.visibilityOf(filter_textbox));
+            Util.Action().moveToElement(filter_textbox).perform();
+            Util.Action().click().perform();
+            
+            if(filterName.equals("User")){
+        	    Util.Action().sendKeys(filterValue.substring(0,filterValue.indexOf('(')-1)).perform();            	
+            }
+            else{
+        	    Util.Action().sendKeys(filterValue).perform();            	
+            }
+
+    	    Util.Action().sendKeys(Keys.ESCAPE).perform();	
+    		
+    		run_button.click();
+    		filter_button.click();
+    		Thread.sleep(5000);
+    		
+    	
+    		List<WebElement> filtered_values = driver.findElements(By.xpath("(//div[@class='ag-grid-container'])[1]//div[@class='ag-center-cols-container']/div/div["+String.valueOf(index)+"]"));
+    		System.out.println("(//div[@class='ag-grid-container'])[1]//div[@class='ag-center-cols-container']/div/div["+String.valueOf(index)+"]");
+    		for(int j=0;j<filtered_values.size();j++){
+    			
+    			System.out.println(filtered_values.get(j).getText());
+    			System.out.println(filterValue);
+    			logger.log(LogStatus.INFO, "Verifying if "+filtered_values.get(j).getText()+" is matching with "+filterValue);
+    			softassert.assertTrue(filtered_values.get(j).getText().equals(filterValue),"value "+filtered_values.get(j).getText()+" is not filtered value");
+    		}
+    		
+    		softassert.assertAll();
+
+            filter_button.click();
+    		
+    		wait.until(ExpectedConditions.visibilityOf(filter_textbox));
+            Util.Action().moveToElement(filter_textbox).perform();
+            Util.Action().click().perform();
+            Thread.sleep(1000);
+    	    Util.Action().sendKeys(Keys.BACK_SPACE).perform();
+            Util.Action().sendKeys(Keys.BACK_SPACE).perform();
+    		Util.Action().sendKeys(Keys.ESCAPE).perform();	
+    	
+    		filter_button.click();	        
+	}	    
+    
+    
+    
+    
+    
 }
