0ce50059cebacabb9bfd5933ce4a371a4d72a642
sagarjadhav180 <sagar.j@moentek.com>
2020-06-04 10:51:43 +0530

    Looker report test scripts added

---
+++ replica/src/test/java/tests/AnalyticsSummaryReportTest.java
@@ -0,0 +1,98 @@
+package tests;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import constants.Constants;
+import pom.CallLogsReportPage;
+import pom.AnalyticsSummaryReportPage;
+import pom.HomePage;
+import pom.LoginPage;
+
+public class AnalyticsSummaryReportTest extends TestBase{
+
+	HomePage hp;
+	CallLogsReportPage clr;
+	AnalyticsSummaryReportPage ts; 
+	
+	@BeforeClass
+	public void goToLookerReports() throws Exception{
+		LoginPage lp=new LoginPage(driver);
+		logger=extent.startTest("validLogin"); 
+        logger.log(LogStatus.INFO, "verifying login with valid username_and_password. ");
+        lp.validLogin();
+        
+        goToReport();
+
+	}
+
+	public void goToReport() throws Exception{
+		ts=new AnalyticsSummaryReportPage(driver);
+		clr=new CallLogsReportPage(driver);
+		hp=new HomePage(driver);
+		hp.clickAction("Reports");
+		Thread.sleep(1000);
+		clr.goToReport(Constants.LookerReports.analytics_summary_report);
+        Thread.sleep(7000);
+		ts.switchToIFrame();
+	}
+	
+	@Test(priority=1)
+	public void runButtonVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Run Button Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.runButton();
+	}
+	
+	@Test(priority=2)
+	public void headerLabelVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Header Label Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.headerLabel();
+	}
+	
+	@Test(priority=3)
+	public void presenceOfGearIconVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Presence Of Gear Icon Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.presenceOfGearIcon();
+	}
+	
+	@Test(priority=4)
+	public void gearIconOptionsVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Gear Icon options Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+        ts.gearIconOptions();
+	}
+	
+	@Test(priority=5)
+	public void presenceOfTimeZoneVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Presence Of Time Zone Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.presenceOfTimeZone();
+	}
+	
+	
+	@AfterClass
+	public void loggingOut() throws InterruptedException{
+		
+		ts=new AnalyticsSummaryReportPage(driver);
+		ts.switchToMainWindow();
+		
+		LoginPage lp=new LoginPage(driver);
+        logger=extent.startTest("LogOut"); 
+        logger.log(LogStatus.INFO, "loggin out.. ");
+        lp.logOut();
+	}
+	
+}
