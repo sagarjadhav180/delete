abc0c8e11e9c14943599af8cdf2a69b4f430eb1a
sagarjadhav180 <sagar.j@moentek.com>
2020-06-05 15:40:55 +0530

    minor changes

--- replica/src/test/java/tests/AnalyticsSummaryReportTest.java
+++ replica/src/test/java/tests/AnalyticsSummaryReportTest.java
@@ -10,6 +10,7 @@ import com.relevantcodes.extentreports.LogStatus;
 
 import constants.Constants;
 import pom.CallLogsReportPage;
+import pom.CallTrendsReportPage;
 import pom.AnalyticsSummaryReportPage;
 import pom.HomePage;
 import pom.LoginPage;
@@ -81,7 +82,70 @@ public class AnalyticsSummaryReportTest extends TestBase{
         ts=new AnalyticsSummaryReportPage(driver);
 		ts.presenceOfTimeZone();
 	}
+
+	@Test(priority=6)
+	public void tilesVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Tiles Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.tilesVerification();
+	}
 	
+	@Test(priority=7)
+	public void totalCallsCountVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Total Calls Count Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.tileValueVerificationForDefault7DaysFilter(Constants.LookerAnalyticsSummaryReport.total_call_tile);
+	}
+	
+	@Test(priority=8)
+	public void analyzedCallsCountVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Analyzed Calls Count Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.tileValueVerificationForDefault7DaysFilter(Constants.LookerAnalyticsSummaryReport.total_analyzed_calls_tile);
+	}
+	
+	@Test(priority=9)
+	public void presenceOfIndicatorGraphVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Presence Of Indicator Graph Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.hitMissGraph();
+	}
+	
+	@Test(priority=10)
+	public void presenceOfIndicatorsVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Presence Of Indicators Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.hitMissGraphindicatorVerification();
+	}
+	
+	@Test(priority=11)
+	public void hitMissTableColumnVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Hit Miss Table Column Verification Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.hitMixTableColumnVerification();
+	}
+	
+	@Test(priority=12)
+	public void filterButtonTest() throws IOException, InterruptedException{
+		logger=extent.startTest("Filter Button Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.filterButton();
+	}
+	
+	@Test(priority=13)
+	public void filterButtonElementsTest() throws IOException, InterruptedException{
+		logger=extent.startTest("Filter Button Elements Test"); 
+		logger.assignCategory(Constants.analytics_summary_category);
+        ts=new AnalyticsSummaryReportPage(driver);
+		ts.filterElements();
+	}
 	
 	@AfterClass
 	public void loggingOut() throws InterruptedException{
