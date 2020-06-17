2b641fd46fa151ea62cab73af705c28e8710cfff
sagarjadhav180 <sagar.j@moentek.com>
2020-06-17 18:28:58 +0530

    Looker report test scripts added

--- replica/src/test/java/tests/LookerAccountDetailsReportTest.java
+++ replica/src/test/java/tests/LookerAccountDetailsReportTest.java
@@ -82,7 +82,8 @@ public class LookerAccountDetailsReportTest extends TestBase{
         ts=new AccountDetailsReportPage(driver);
 		ts.presenceOfTimeZone();
 	}
-	
+
+	@Test(priority=6)
 	public void presenceOfGroupSettingsLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Group Settings Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -90,6 +91,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.groupSettingsLabel();
 	}
 	
+	@Test(priority=7)
 	public void groupSettingsTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Group Settings Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -97,6 +99,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.groupSettingsTableColumnVerification();
 	}
 	
+	@Test(priority=8)
 	public void presenceOfCampaignSettingsLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Campaign Settings Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -104,6 +107,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.campaignSettingsLabel();
 	}
 	
+	@Test(priority=9)
 	public void campaignsSettingsTilesVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Campaigns Settings Tiles Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -111,6 +115,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
         ts.campaignSettingstilesVerification();
 	}
 	
+	@Test(priority=10)
 	public void activeCampaignsVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Active Campaigns Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -118,6 +123,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
         ts.campaignAndTNTileValueVerification(Constants.AccountDetailsReport.active_campaigns_tile);
 	}
 	
+	@Test(priority=11)
 	public void inActiveCampaignsVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Inactive Campaigns Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -125,13 +131,15 @@ public class LookerAccountDetailsReportTest extends TestBase{
         ts.campaignAndTNTileValueVerification(Constants.AccountDetailsReport.inactive_campaigns_tile);
 	}
 	
+	@Test(priority=12)
 	public void campaignSettingsTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Campaign Settings Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
         ts=new AccountDetailsReportPage(driver);
 		ts.campaignSettingsTableColumnVerification();
 	}
-	
+
+	@Test(priority=13)
 	public void presenceOfTNSettingsLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Tracking number Settings Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -139,6 +147,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.tnSettingsLabel();
 	}
 	
+	@Test(priority=14)
 	public void trackingNumberSettingsTilesVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Tracking Number Settings Tiles Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -146,6 +155,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
         ts.tnSettingstilesVerification();
 	}
 	
+	@Test(priority=15)
 	public void activeTrackingNumberVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Active Tracking Number Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -153,6 +163,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
         ts.campaignAndTNTileValueVerification(Constants.AccountDetailsReport.active_tracking_numbers_tile);
 	}
 	
+	@Test(priority=16)
 	public void inActiveTrackingNumberVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Inactive Tracking Number Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -160,6 +171,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
         ts.campaignAndTNTileValueVerification(Constants.AccountDetailsReport.inactive_tracking_numbers_tile);
 	}
 	
+	@Test(priority=17)
 	public void trackingNumberSettingsTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Tracking Number Settings Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -167,6 +179,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.tnSettingsTableColumnVerification();
 	}
 	
+	@Test(priority=18)
 	public void presenceOfBillingUsageLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Billing Usage Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -174,6 +187,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.billingUsageLabel();
 	}
 	
+	@Test(priority=19)
 	public void billingUsagaeTilesVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Billing Usagae Tiles Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -181,6 +195,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
         ts.billingUsagetilesVerification();
 	}
 	
+	@Test(priority=20)
 	public void presenceOfUsageSummaryByGroupLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Usage Summary By Group Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -188,6 +203,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.usageSummaryByGroupLabel();
 	}
 	
+	@Test(priority=21)
 	public void billingUsageByGroupTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Billing Usage By Group Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -195,13 +211,15 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.usageSummaryByGroupTableColumnVerification();
 	}
 	
+	@Test(priority=22)
 	public void presenceOfConfigSummaryByGroupLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Config Summary By Group Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
         ts=new AccountDetailsReportPage(driver);
 		ts.configurationSummaryByGroupLabel();
 	}
-	
+
+	@Test(priority=23)
 	public void configSummaryByGroupTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Config Summary By Group Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -209,6 +227,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.configSummaryByGroupTableColumnVerification();
 	}
 	
+	@Test(priority=24)
 	public void presenceOfUsageByTrackingNumberLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Usage By Tracking Numner Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -216,6 +235,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.usageByTrackingNumberLabel();
 	}
 	
+	@Test(priority=25)
 	public void UsageByTrackingNumberTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Usage By Tracking Number Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -223,6 +243,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.usageByTrackingNumberTableColumnVerification();
 	}
 	
+	@Test(priority=26)
 	public void presenceOfCallTrendsLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of call Trends Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -230,13 +251,15 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callTrendsLabel();
 	}
 	
-	public void tilesVerification() throws IOException, InterruptedException{
-		logger=extent.startTest("Tiles Verification Test"); 
+	@Test(priority=27)
+	public void callTrendTilesVerification() throws IOException, InterruptedException{
+		logger=extent.startTest("Call Trends Tiles Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
         ts=new AccountDetailsReportPage(driver);
-		ts.tilesVerification();
+		ts.callTrendsTilesVerification();
 	}
 	
+	@Test(priority=28)
 	public void totalCallsCountVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Total Calls Count Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -244,6 +267,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callTrendsTileValueVerificationForDefault7DaysFilter(Constants.AccountDetailsReport.total_calls_tile);
 	}
 	
+	@Test(priority=29)
 	public void uniqueCallsCountVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Unique Calls Count Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -251,13 +275,15 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callTrendsTileValueVerificationForDefault7DaysFilter(Constants.AccountDetailsReport.unique_calls_tile);
 	}
 	
+	@Test(priority=30)
 	public void answeredCallsCountVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Answered Calls Count Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
         ts=new AccountDetailsReportPage(driver);
 		ts.callTrendsTileValueVerificationForDefault7DaysFilter(Constants.AccountDetailsReport.answered_calls_tile);
 	}
-	
+
+	@Test(priority=31)
 	public void unAnsweredCallsCountVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Unanswered Calls Count Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -265,6 +291,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callTrendsTileValueVerificationForDefault7DaysFilter(Constants.AccountDetailsReport.unanswered_calls_tile);
 	}
 	
+	@Test(priority=32)
 	public void longestCallDurationVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Longest Call Duration Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -272,7 +299,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callTrendsTileValueVerificationForDefault7DaysFilter(Constants.AccountDetailsReport.longest_call_duration_tile);
 	}
 	
-
+	@Test(priority=33)
 	public void averageCallDurationVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Average Call Duration Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -280,6 +307,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callTrendsTileValueVerificationForDefault7DaysFilter(Constants.AccountDetailsReport.average_calls_duration_tile);
 	}
 	
+	@Test(priority=34)
 	public void presenceOfCallsOverTimebyDayLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Calls Over Time by Day Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -287,6 +315,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callsOverTimebyDayLabel();
 	}
 	
+	@Test(priority=35)
 	public void presenceOfCallsOverTimebyDayGraphVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Calls Over Time by Day Graph Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -294,6 +323,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callsOverTimebyDayGraph();
 	}
 	
+	@Test(priority=36)
 	public void callsOverTimebyDayTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Calls Over Time by Day Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -301,6 +331,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callsOverTimebyDayTableColumnVerification();
 	}
 
+	@Test(priority=37)
 	public void presenceOfCallsOverTimebyHourLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Calls Over Time by Hour Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -308,6 +339,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callsOverTimebyHourLabel();
 	}
 	
+	@Test(priority=38)
 	public void presenceOfCallsOverTimebyHourGraphVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Calls Over Time by Hour Graph Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -315,6 +347,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callsOverTimebyHourGraph();
 	}
 	
+	@Test(priority=39)
 	public void callsOverTimebyHourTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Calls Over Time by Hour Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -322,13 +355,15 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callsOverTimebyHourTableColumnVerification();
 	}
 	
+	@Test(priority=40)
 	public void presenceOfCallMixLabelVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Call Mix Label Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
         ts=new AccountDetailsReportPage(driver);
 		ts.callsMixLabel();
 	}
-	
+
+	@Test(priority=41)
 	public void presenceOfCallMixGraphVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Presence Of Call Mix Graph Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -336,6 +371,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callMixGraph();
 	}
 
+	@Test(priority=42)
 	public void callMixTableColumnVerification() throws IOException, InterruptedException{
 		logger=extent.startTest("Call Mix Table Column Verification Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -343,6 +379,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.callMixTableColumnVerification();
 	}
 
+	@Test(priority=43)
 	public void filterButtonTest() throws IOException, InterruptedException{
 		logger=extent.startTest("Filter Button Test"); 
 		logger.assignCategory(Constants.account_details_category);
@@ -350,6 +387,7 @@ public class LookerAccountDetailsReportTest extends TestBase{
 		ts.filterButton();
 	}
 	
+	@Test(priority=44)
 	public void filterButtonElementsTest() throws IOException, InterruptedException{
 		logger=extent.startTest("Filter Button Elements Test"); 
 		logger.assignCategory(Constants.account_details_category);
