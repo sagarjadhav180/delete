07e2534a486cbaae2164474f74fe96038a810b55
sagar <sagar.jadhav180@gmail.com>
2020-07-24 17:13:36 +0530

    Tests added for Groups and User page

--- replica/src/test/java/tests/GroupsAndUserTest.java
+++ replica/src/test/java/tests/GroupsAndUserTest.java
@@ -1,6 +1,86 @@
 package tests;
 
-public class GroupsAndUserUIVerificationTest extends TestBase
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import constants.Constants;
+import pom.CampaignAndTrackingNumberPage;
+import pom.CampaignBuilderPage;
+import pom.GroupsAndUserPage;
+import pom.HomePage;
+import pom.LoginPage;
+
+public class GroupsAndUserTest extends TestBase
 {
+	
+	    //Logging In
+		@BeforeClass
+		public void goToGroupsAndUserPage() throws IOException, InterruptedException{
+			
+			LoginPage lp=new LoginPage(driver);
+	        lp.validLogin();
+
+	        HomePage hp=new HomePage(driver);
+	        
+			try {
+				hp.clickAction(Constants.HomePage.group_User_page);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+			
+		}
+		
+		
+		//Check if Group and User label is displayed
+		@Test(priority=1)
+		public void groupsAndUserPageLabel() throws InterruptedException{
+			
+	        logger=extent.startTest("Groups And User Page Label verification..");
+			logger.assignCategory(Constants.groups_and_user_category);
+			
+	        GroupsAndUserPage gp=new GroupsAndUserPage(driver);
+	        gp.groupsAndUserHeaderLabel();
+		}
+		
+		//Check UI of Users section
+		@Test(priority=2)
+		public void userSectionUIVerification() throws InterruptedException{
+			
+	        logger=extent.startTest("User Section UI Verification verification..");
+			logger.assignCategory(Constants.groups_and_user_category);
+			
+	        GroupsAndUserPage gp=new GroupsAndUserPage(driver);
+	        gp.users_UI_Validation();
+		}
+		
+		
+		
+		
+		//Logging Out
+		@AfterClass
+		public void goToHomePage(){
+	        
+			HomePage hp=new HomePage(driver);
+	        Util.getJavascriptExecutor().executeScript("window.scrollBy(0,-2000)");	
+	        try {
+				hp.clickAction("Home");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        LoginPage lp=new LoginPage(driver);
+	    
+	        logger.log(LogStatus.INFO, "loggin out.. ");
+	        lp.logOut();
+	        
+		}
+		
+	
 
 }
