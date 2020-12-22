f7c9503c0b03924e5f75c9c4c1350ccd1b5856cb
sagar <sagar.jadhav180@gmail.com>
2020-12-22 18:17:31 +0530

    minor changes

--- replica/src/test/java/tests/TestBase.java
+++ replica/src/test/java/tests/TestBase.java
@@ -153,7 +153,7 @@ public class TestBase
 		driver.manage().window().maximize();
 		//to delete cookies
 	    driver.manage().deleteAllCookies();
-		String URL="https://convirza.awsapps.com/auth/?client_id=06919f4fd8ed324e&redirect_uri=https%3A%2F%2Fconvirza.awsapps.com%2Fconnect%2Fauth%2Fcode";
+//		String URL="https://convirza.awsapps.com/auth/?client_id=06919f4fd8ed324e&redirect_uri=https%3A%2F%2Fconvirza.awsapps.com%2Fconnect%2Fauth%2Fcode";
 	    
 		Properties prop=new Properties();
 		
