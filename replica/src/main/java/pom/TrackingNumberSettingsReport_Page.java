2f83efbac28f825a9223aaee78ff10b5c7fcc392
sagar <sagar.jadhav180@gmail.com>
2021-01-19 12:58:45 +0530

    minor change in query -- tn count

--- replica/src/main/java/pom/TrackingNumberSettingsReport_Page.java
+++ replica/src/main/java/pom/TrackingNumberSettingsReport_Page.java
@@ -188,7 +188,7 @@ public class TrackingNumberSettingsReport_Page extends TestBase{
     	SoftAssert softassert=new SoftAssert();
     	//verification of count in pagination toolbox	
 
-		String dbCount = Util.readingFromDB("SELECT count(*) as count FROM ce_call_flows WHERE ouid IN (SELECT org_unit_id FROM org_unit WHERE top_ou_id ='"+TestBase.getOrg_unit_id()+"') AND status='active'");
+		String dbCount = Util.readingFromDB("SELECT count(*) as count FROM ce_call_flows WHERE ouid IN (SELECT org_unit_id FROM org_unit WHERE top_ou_id ='"+TestBase.getOrg_unit_id()+"') AND status IN ('active','inactive','referral')");
 
 
 		String countOnUI_pagination = pagination_call_count_label.getText().substring(pagination_call_count_label.getText().indexOf('f')+2);
@@ -205,7 +205,7 @@ public class TrackingNumberSettingsReport_Page extends TestBase{
     public void tableTrackingNumbersCount(){
 		//verification of count in pagination toolbox	
     	SoftAssert softassert=new SoftAssert();
-		String dbCount = Util.readingFromDB("SELECT count(*) as count FROM ce_call_flows WHERE ouid IN (SELECT org_unit_id FROM org_unit WHERE top_ou_id ='"+TestBase.getOrg_unit_id()+"') AND status='active'");
+		String dbCount = Util.readingFromDB("SELECT count(*) as count FROM ce_call_flows WHERE ouid IN (SELECT org_unit_id FROM org_unit WHERE top_ou_id ='"+TestBase.getOrg_unit_id()+"') AND status IN ('active','inactive','referral')");
 
 		int final_count=table_call_count.size()+0;
 		Util.scrollFunction(next_100_button);
