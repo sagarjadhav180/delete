a4470526bb3fe83f02c13ac1572df3c4d4d964b4
sagar <sagar.jadhav180@gmail.com>
2021-02-04 20:33:15 +0530

    new class added to to store db connecction params

---
+++ replica/src/test/java/dbUtil/DBConnectionParameters.java
@@ -0,0 +1,12 @@
+package dbUtil;
+
+public class DBConnectionParameters {
+
+	public static final String read_only_user = "";
+	public static final String read_only_pass = "";
+	public static final String read_and_write_user = "moentekdbrw";
+	public static final String read_and_write_password = "hyPdua14GAu6";
+	public static final String staging_db = "ct_stg";
+	public static final String pg_driver = "org.postgresql.Driver";
+	
+}
