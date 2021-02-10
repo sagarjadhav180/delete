854ed20308550f9d20dc11d8af9ae278a33860aa
sagar <sagar.jadhav180@gmail.com>
2021-02-10 11:56:10 +0530

    new function added to update app prop

--- replica/src/test/java/io/EnvironmentHelper.java
+++ replica/src/test/java/io/EnvironmentHelper.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 
 import dbUtil.DBConnectionParameters;
 import tests.TestBase;
+import tests.Util;
 
 public class EnvironmentHelper {
 
@@ -25,12 +26,6 @@ public class EnvironmentHelper {
 		FileOutputStream fis=new FileOutputStream(new File(".//property"));
 		prop.store(fis, "Env");
 		
-		String user=prop.getProperty("username");
-		TestBase.setUser_id(user);
-		String pass=prop.getProperty("password");
-		TestBase.setPassword(pass);
-		String environment = prop.getProperty("Environment");
-		TestBase.setEnv(environment);
 	}
 	
 	public void updateDBConfig() throws IOException{
@@ -40,10 +35,27 @@ public class EnvironmentHelper {
 		prop.setProperty("username", DBConnectionParameters.read_and_write_user);
 		prop.setProperty("password", DBConnectionParameters.read_and_write_password);
 		prop.setProperty("driver", DBConnectionParameters.pg_driver);
-		prop.setProperty("connection_url", "jdbc:postgresql://"+TestBase.getEnv()+"-pg-1.convirza.com:5432");
+		prop.setProperty("connection_url", "jdbc:postgresql://"+System.getProperty("Environment")+"-pg-1.convirza.com:5432");
 		
 		FileOutputStream fis=new FileOutputStream(new File(".//PostgresConfig.properties"));
 		prop.store(fis, "DB");
+	}
+	
+	public void updateTestBaseProperties() throws IOException {
+		Properties prop=new Properties();
+		FileInputStream file=new FileInputStream(".//property");
+		prop.load(file);
+		String user=prop.getProperty("username");
+		TestBase.setUser_id(user);
+		String pass=prop.getProperty("password");
+		TestBase.setPassword(pass);
+		String environment = prop.getProperty("Environment");
+		TestBase.setEnv(environment);
+		String group=Util.readingFromDB("SELECT ct_user_ou_id AS count FROM ct_user WHERE username LIKE '"+TestBase.getUser_id()+"'");
+		System.out.println("group "+group);
+		TestBase.setOrg_unit_id(group);
+		TestBase.setCampaign_ou_id(group);
+		
 	}
 	
 }
