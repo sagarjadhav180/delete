fa097228c1a885959b84d95246332c25a21b07ef
sagar <sagar.jadhav180@gmail.com>
2021-02-11 20:42:16 +0530

    new class added for utility constants

---
+++ replica/src/test/java/utilConstants/UtilityConstants.java
@@ -0,0 +1,17 @@
+package utilConstants;
+
+public class UtilityConstants {
+
+	
+	  public static class TimeConsatnts{
+	    	
+	    	public static final String date_range_for_today = "today";
+	    	public static final String date_range_for_yesterday = "yesterday";
+	    	public static final String date_range_for_last_7_days = "last 7 days";
+	    	public static final String date_range_for_last_30_days = "last 30 days";
+	    	public static final String date_range_for_this_month = "this month";
+	    	public static final String date_range_for_last_month = "last month";
+	    	
+	    }
+
+}
