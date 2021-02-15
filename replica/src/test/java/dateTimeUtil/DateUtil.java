d8737532306cf48079fce90ecc4864a14ee482ba
sagar <sagar.jadhav180@gmail.com>
2021-02-15 19:04:25 +0530

    minor changes

--- replica/src/test/java/dateTimeUtil/DateUtil.java
+++ replica/src/test/java/dateTimeUtil/DateUtil.java
@@ -12,33 +12,33 @@ public class DateUtil {
 		Map<String, String> dateRange = new HashMap<String, String>();
 		
 		switch(range) {
-		case "today":
+		case "Today":
 		    String endDateToBeUsedForToday = Util.getDate("yyyy-MM-dd","1");
 			String startDateToBeUsedForToday = Util.getDate("yyyy-MM-dd","0");
 			dateRange.put("endDateToBeUsed", endDateToBeUsedForToday);
 			dateRange.put("startDateToBeUsed", startDateToBeUsedForToday);
 			break;
-		case "yesterday":
+		case "Yesterday":
 		    String endDateToBeUsedForYesterday = Util.getDate("yyyy-MM-dd","-1");
 			String startDateToBeUsedForYesterday = Util.getDate("yyyy-MM-dd","0");
 			dateRange.put("endDateToBeUsed", endDateToBeUsedForYesterday);
 			dateRange.put("startDateToBeUsed", startDateToBeUsedForYesterday);
 			break;
-		case "last 7 days":
+		case "Last 7 Days":
 		    String endDateToBeUsedForLast7Days = Util.getDate("yyyy-MM-dd","1");
 			String startDateToBeUsedForLast7Days = Util.getDate("yyyy-MM-dd","-6");
 			dateRange.put("endDateToBeUsed", endDateToBeUsedForLast7Days);
 			dateRange.put("startDateToBeUsed", startDateToBeUsedForLast7Days);
 			break;
-		case "last 30 days":
+		case "Last 30 Days":
 		    String endDateToBeUsedForLast30Days = Util.getDate("yyyy-MM-dd","1");
 			String startDateToBeUsedForLast30Days = Util.getDate("yyyy-MM-dd","-29");
 			dateRange.put("endDateToBeUsed", endDateToBeUsedForLast30Days);
 			dateRange.put("startDateToBeUsed", startDateToBeUsedForLast30Days);
 			break;
-		case "this month":
+		case "This Month":
 			break;		
-		case "last month":
+		case "Last Month":
 			break;			
 		}
 		return dateRange;
